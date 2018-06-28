c7cf167c04b3224f96164ae54df4ea8d54c704b0
Suchita Mistry <smistry@fb.com>
2018-06-27 22:00:22 -0700

    Rounded corners

--- app/src/main/java/com/example/smistry/flicks/MovieAdapter.java
+++ app/src/main/java/com/example/smistry/flicks/MovieAdapter.java
@@ -9,25 +9,39 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.example.smistry.flicks.models.Config;
 import com.example.smistry.flicks.models.Movie;
 
 import java.util.ArrayList;
 
+
 public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
     //list of movies
     ArrayList<Movie> movies;
+    //config needed for image url
+    Config config;
+    //context for rendering
+    Context context;
 
     //initialize with list
-
     public MovieAdapter(ArrayList<Movie> movies) {
         this.movies = movies;
+    }
+
+    public Config getConfig() {
+        return config;
+    }
+
+    public void setConfig(Config config) {
+        this.config = config;
     }
 
     //creates and inflates a new view
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         //get the context and create the inflater
-        Context context = parent.getContext();
+        context = parent.getContext();
         LayoutInflater inflater = LayoutInflater.from(context);
         //create the view using the the item_movie layout
         View movieView = inflater.inflate(R.layout.item_movie, parent, false);
@@ -44,7 +58,17 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
         holder.tvTitle.setText(movie.getTitle());
         holder.tvOverview.setText(movie.getOverview());
 
-        //TODO-set image using glide
+        //build url for poster image
+        String imageUrl = config.getImageUrl(config.getPosterSize(), movie.getPosterPath());
+
+       //load image using glide
+        GlideApp.with(context)
+                .load(imageUrl)
+                .transform(new RoundedCorners(15))
+                .placeholder(R.drawable.flicks_movie_placeholder)
+                .error(R.drawable.flicks_movie_placeholder)
+                .into(holder.ivPosterImage);
+
 
     }
 
--- app/src/main/java/com/example/smistry/flicks/MovieListActivity.java
+++ app/src/main/java/com/example/smistry/flicks/MovieListActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.example.smistry.flicks.models.Config;
 import com.example.smistry.flicks.models.Movie;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
@@ -32,16 +33,14 @@ public class MovieListActivity extends AppCompatActivity {
 
     //instance fields
     AsyncHttpClient client;
-    //the base url for loading images
-    String imageBaseUrl;
-    //the poster size to use when fetching images, part of the url
-    String posterSize;
     // the list of currently playing movies
     ArrayList<Movie> movies;
     //the recycler view
     RecyclerView rvMovies;
     //the adapter wired to the recycler view
     MovieAdapter adapter;
+    //image confige
+    Config config;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -114,14 +113,13 @@ public class MovieListActivity extends AppCompatActivity {
                // super.onSuccess(statusCode, headers, response);
                 //get the image base url
                 try {
-                    JSONObject images = response.getJSONObject("images");
-                    //get the image base url
-                    imageBaseUrl = images.getString("secure_base_url");
-                    //get the poster size
-                    JSONArray posterSizeOptions = images.getJSONArray("poster_sizes");
-                    //use the option at index 3 or w342 as fallback
-                    posterSize = posterSizeOptions.optString(3, "w342");
-                    Log.i(TAG, String.format("Loaded configuration with imageBaseUrl %s and posterSize %s", imageBaseUrl, posterSize));
+                    config = new Config(response);
+                    Log.i(TAG,
+                            String.format("Loaded configuration with imageBaseUrl %s and posterSize %s",
+                                    config.getImageBaseUrl(),
+                                    config.getPosterSize()));
+                    //pass config to adapter
+                    adapter.setConfig(config);
                     // get the now playing movie list
                     getNowPlaying();
                 } catch (JSONException e) {
